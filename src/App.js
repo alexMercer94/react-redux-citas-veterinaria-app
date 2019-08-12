a3d91a02e41e526a7fa149668fa53371a24fd152
Anzen Digital <alex.c.valera94@hotmail.com>
2019-08-12 11:51:07 -0500

    Basic structure

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="container">
+            <header>
+                <h1 className="text-center">Administrador de Pacientes de Veterinaria</h1>
+            </header>
+            <div className="row mt-3">
+                <div className="col-md-6">Formulario</div>
+                <div className="col-md-6">Listado</div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
